9fdd04b9361bc19d7a2765bbb6e60b30b78ff428
Yoran515 <yoran.vis2004@gmail.com>
2024-06-05 13:49:33 +0200

    Tijd Toegevoegd

---
+++ Assets/Scripts/TimeVideo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class TimeVideo : MonoBehaviour
+{
+    public float time;
+    public TMP_Text m_TextMeshPro;
+
+    public GameObject Laser;
+    public GameObject Danger1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Danger1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TextMeshPro.SetText(time.ToString());
+        time += Time.deltaTime;
+
+        if(time > 7)
+        {
+            Danger1.SetActive(true);
+        }
+    }
+        
+    
+}
